b2de7806028184e4773125131995ba4e31dd40d8
Forgezz <60566488+Forgezz@users.noreply.github.com>
2025-01-19 23:41:07 +0800

    New feature: Diarization

--- transcript.py
+++ transcript.py
@@ -1,3 +1,4 @@
+# transcript.py
 import whisper
 import gc
 import os
@@ -7,7 +8,7 @@ def load_whisper_model_v3(model_name="large-v3"):
     Load the Whisper model (large-v3) from Hugging Face.
     """
     print(f"Loading Whisper model: {model_name}...")
-    model = whisper.load_model(model_name)
+    model = whisper.load_model("large-v3").to("cuda")  # 强制模型加载到 GPU
     print(f"Model {model_name} loaded successfully.")
     return model
 
@@ -16,9 +17,13 @@ def transcribe_audio_v3(model, audio_path, task="transcribe", language=None,verb
     """
     Transcribe audio using Whisper model (large-v3).
     """
+    
     print(f"Transcribing audio: {audio_path}...")
-    result = model.transcribe(audio_path, task=task, language=language, verbose=verbose)#增加了最后一个verbose
+    
+    result = model.transcribe(audio_path, task=task, language=language, verbose=verbose)
+    
     print("Transcription completed.")
+    
     return result
 
 
@@ -51,3 +56,16 @@ def format_timestamp(seconds):
     return f"{hours:02}:{minutes:02}:{int(seconds):02},{milliseconds:03}"
 
 
+# 纯文本文件
+def save_transcription_as_txt_v3(transcription, output_path):
+    """
+    Save transcription result as a plain text file without timestamps.
+    """
+    text_file_path = os.path.splitext(output_path)[0] + ".txt"
+    with open(text_file_path, "w", encoding="utf-8") as text_file:
+        full_text = ""
+        for segment in transcription['segments']:
+            full_text += segment['text']
+        text_file.write(full_text)
+    print(f"Transcription saved as TXT: {text_file_path}")
+    return text_file_path
